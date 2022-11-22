ed60ed23139d32428d32441826c456444cd3a848
Msahu201989 <Msahu201989@gmail.com>
2022-11-22 20:44:35 +0530

    test instance create using resource and provider

--- 03-resources/02/ec2.tf
+++ 03-resources/02/ec2.tf
@@ -3,6 +3,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "sample" {
-  ami = "ami-00d48a21603b2119b"
+  ami           = "ami-00d48a21603b2119b"
   instance_type = "t3.micro"
 }
+
+output "ec2_public_ip" {
+  value = aws_instance.sample.public_ip
+}
+

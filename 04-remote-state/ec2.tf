98ceb44f1523bf8edd93763c4201c795cf250e59
Msahu201989 <Msahu201989@gmail.com>
2022-11-23 20:18:58 +0530

    tag name of server using function

--- 04-remote-state/ec2.tf
+++ 04-remote-state/ec2.tf
@@ -2,5 +2,13 @@ resource "aws_instance" "sample" {
   ami           = "ami-00d48a21603b2119b"
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    name = upper(var.tagName)
+  }
 }
+
+variable "tagName" {}
+
+
 

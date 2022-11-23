526fce54a77751b0515f4639d2f3b560efb539bc
Msahu201989 <Msahu201989@gmail.com>
2022-11-23 20:46:35 +0530

    terraform datasource ami details

--- 05-functions/main.tf
+++
@@ -1,9 +0,0 @@
-data "aws_ami" "example" {
-   most_recent = true
-   name_regex = "Centos-8-DevOps-Practice"
-   owners     = ["973714476881"]
- }
-
-output "ami" {
-  value = data.aws_ami.example
-}

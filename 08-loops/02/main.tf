8eebd79895749f733269b8c1157135590a22bf39
Msahu201989 <Msahu201989@gmail.com>
2022-11-24 13:43:37 +0530

    Loops count 2

--- 08-loops/02/main.tf
+++ 08-loops/02/main.tf
@@ -1,12 +1,17 @@
 resource "aws_instance" "sample" {
-  count = 2
+  count         = length(var.instances)
   ami           = "ami-00d48a21603b2119b"
   instance_type = "t3.micro"
   tags = {
-    Name = "test-${count.index}"
+    Name = element(var.instances, count.index)
   }
 }
 
 output "public_ip" {                        #Print public IP
   value = aws_instance.sample.*.public_ip   # We use .*. if we have multiple instance
 }
+
+variable "instances" {
+  default = ["cart", "catalogue"]
+}
+

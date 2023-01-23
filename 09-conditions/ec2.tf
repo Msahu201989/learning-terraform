7a2e037ba586396af18e38c588d3ecb09a5ef3ba
Mukesh <msahu201989@gmail.com>
2023-01-23 20:11:30 +0530

    terraform_learning_devops

--- 09-conditions/ec2.tf
+++ 09-conditions/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "sample" {
-  ami           = "ami-00d48a21603b2119b"
+  ami           = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
   vpc_security_group_ids = aws_security_group.allow_ssh.*.id
 

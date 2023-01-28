dcaf7c9bc1ef1656e98006e94f73aa4c3407e18f
Mukesh <Stunty@007>
2023-01-28 19:50:02 +0530

    learning prometheus

--- 03-resources/02/ec2.tf
+++ 03-resources/02/ec2.tf
@@ -6,12 +6,12 @@ resource "aws_instance" "sample" {
   ami           = "ami-00d48a21603b2119b"
   instance_type = "t3.micro"
 
-  root_block_device {
-    volume_size = 15
-  }
-}
-
-output "ec2_public_ip" {
-  value = aws_instance.sample.public_ip
-}
+#  root_block_device {
+#    volume_size = 15
+#  }
+#}
+#
+#output "ec2_public_ip" {
+#  value = aws_instance.sample.public_ip
+#}
 

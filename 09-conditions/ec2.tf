4454c1c318f19d3ff250b1fa343344fbf6b70cf8
Mukesh <Stunty@007>
2023-01-31 20:06:02 +0530

    learning devops

--- 09-conditions/ec2.tf
+++ 09-conditions/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "sample" {
-  ami           = "ami-00d48a21603b2119b"
+  ami           = "ami-0bb6af715826253bf"
   instance_type = "t3.micro"
   vpc_security_group_ids = aws_security_group.allow_ssh.*.id
 

e1e53c0d51804a53a2a2a26208ff320417fd7933
Mukesh <Stunty@007>
2023-05-12 20:11:28 +0530

    learning devops

--- 02-variables/var.tf
+++ 02-variables/var.tf
@@ -2,7 +2,13 @@ variable "sample" {
   default = "hello"
 }
 
+output "sample" {
+  value = "var.sample"
+}
 
+output "sample1" {
+  value = "hey, ${var.sample}"
+}
 
 #variable "sample" {       # How to declare Variable
 #  default = "hello"

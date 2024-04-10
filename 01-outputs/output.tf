63b0c315dbe1852e4b62d485879e2e8dfc661197
Mukesh.Sahu <msahu201989@gmail.com>
2024-04-10 23:49:11 +0530

    Terraform

--- 01-outputs/output.tf
+++ 01-outputs/output.tf
@@ -1,11 +1,19 @@
-output "sample" {
-  value = "Hello World"
-}
+resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = sg-08f870413c1153e2e
+  subnets            = subnet-03458dba469a5d163
 
-output "sample1" {
-  value = "Hello World"
-}
+  enable_deletion_protection = true
 
-output "sample2" {
-   value = "hello world"
+#  access_logs {
+#    bucket  = aws_s3_bucket.lb_logs.id
+#    prefix  = "test-lb"
+#    enabled = true
+#  }
+
+  tags = {
+    Environment = "production"
+  }
 }

4bbebe3b80765f37e040b3c0e1b0a84395e887b3
Mukesh.Sahu <msahu201989@gmail.com>
2024-04-10 23:58:48 +0530

    Terraform

--- 01-outputs/output.tf
+++ 01-outputs/output.tf
@@ -1,19 +1,34 @@
-resource "aws_lb" "test" {
-  name               = "test-lb-tf"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = sg-08f870413c1153e2e
-  subnets            = subnet-03458dba469a5d163
+resource "aws_lb" "main" {
+  name               = "app-alb"
+  internal           = internal
+  load_balancer_type = application
+  security_groups    = [sg-08f870413c1153e2e]
+  subnets            = [subnet-03458dba469a5d163]
+  #tags               = merge(local.tags, {Name = "${var.env}-alb"})
+}
 
-  enable_deletion_protection = true
+#
+#resource "aws_security_group" "main" {
+#  name        = local.sg_name
+#  description = local.sg_name
+#  vpc_id      =  var.vpc_id
+#  tags         = merge(var.tags, {Name = local.sg_name})
+#
 
-#  access_logs {
-#    bucket  = aws_s3_bucket.lb_logs.id
-#    prefix  = "test-lb"
-#    enabled = true
-#  }
 
-  tags = {
-    Environment = "production"
+  ingress {
+    description = "APP"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }

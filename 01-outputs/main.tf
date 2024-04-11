c27dd66fd746398baedd96d24c55272731634c03
Mukesh.Sahu <msahu201989@gmail.com>
2024-04-11 20:05:47 +0530

    Terraform

--- 01-outputs/main.tf
+++ 01-outputs/main.tf
@@ -6,8 +6,10 @@ resource "aws_lb" "alb_shared_dev" {
 name               = "alb-shared-dev"
 internal           = false
 load_balancer_type = "application"
-security_groups    = ["launch-wizard-2"] // Replace with your security group ID
+security_groups    = ["sg_alb_shared_dev"] // Replace with your security group ID
 subnets            = ["subnet-03458dba469a5d163", "subnet-06d92c264a39f674d"] // Replace with your subnet IDs
+
+enable_deletion_protection = false
 
 listener {
 port            = 443
@@ -29,6 +31,18 @@ protocol        = "HTTP"
 listener {
 port            = 8080
 protocol        = "HTTP"
+}
+}
+
+resource "aws_lb_listener" "alb_shared_dev_http" {
+load_balancer_arn = aws_lb.alb_shared_dev.arn
+port              = 80
+protocol          = "HTTP"
+default_action {
+type             = "fixed-response"
+status_code      = "404"
+content_type     = "text/plain"
+message_body     = "Not Found"
 }
 }
 
@@ -39,6 +53,8 @@ load_balancer_type = "application"
 security_groups    = ["sg_alb_shared_dev"] // Replace with your security group ID
 subnets            = ["subnet-03458dba469a5d163", "subnet-06d92c264a39f674d"] // Replace with your subnet IDs
 
+enable_deletion_protection = false
+
 listener {
 port            = 443
 protocol        = "HTTPS"
@@ -59,5 +75,17 @@ protocol        = "HTTP"
 listener {
 port            = 8080
 protocol        = "HTTP"
+}
+}
+
+resource "aws_lb_listener" "alb_shared_uat_http" {
+load_balancer_arn = aws_lb.alb_shared_uat.arn
+port              = 80
+protocol          = "HTTP"
+default_action {
+type             = "fixed-response"
+status_code      = "404"
+content_type     = "text/plain"
+message_body     = "Not Found"
 }
 }

453852ae22dffc0408495d1cd432b13c195a3751
Mukesh.Sahu <msahu201989@gmail.com>
2024-04-11 20:04:03 +0530

    Terraform

--- 01-outputs/main.tf
+++ 01-outputs/main.tf
@@ -1,67 +1,63 @@
 provider "aws" {
-  region = "your_aws_region"
+region = "your_aws_region"
 }
 
 resource "aws_lb" "alb_shared_dev" {
-  name               = "alb-shared-dev"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = ["launch-wizard-2"] // Replace with your security group ID
-  subnets            = ["subnet-03458dba469a5d163", "subnet-06d92c264a39f674d"] // Replace with your subnet IDs
+name               = "alb-shared-dev"
+internal           = false
+load_balancer_type = "application"
+security_groups    = ["launch-wizard-2"] // Replace with your security group ID
+subnets            = ["subnet-03458dba469a5d163", "subnet-06d92c264a39f674d"] // Replace with your subnet IDs
 
-  enable_deletion_protection = false
+listener {
+port            = 443
+protocol        = "HTTPS"
+ssl_policy      = "ELBSecurityPolicy-2016-08"
+}
 
-  listener {
-    port            = 443
-    protocol        = "HTTPS"
-    ssl_policy      = "ELBSecurityPolicy-2016-08"
-  }
+listener {
+port            = 8099
+protocol        = "HTTPS"
+ssl_policy      = "ELBSecurityPolicy-2016-08"
+}
 
-  listener {
-    port            = 8099
-    protocol        = "HTTPS"
-    ssl_policy      = "ELBSecurityPolicy-2016-08"
-  }
+listener {
+port            = 80
+protocol        = "HTTP"
+}
 
-  listener {
-    port            = 80
-    protocol        = "HTTP"
-  }
-
-  listener {
-    port            = 8080
-    protocol        = "HTTP"
-  }
+listener {
+port            = 8080
+protocol        = "HTTP"
+}
 }
 
 resource "aws_lb" "alb_shared_uat" {
-  name               = "alb-shared-uat"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = ["launch-wizard-2"] // Replace with your security group ID
-  subnets            = ["subnet-03458dba469a5d163", "subnet-06d92c264a39f674d"] // Replace with your subnet IDs
+name               = "alb-shared-uat"
+internal           = false
+load_balancer_type = "application"
+security_groups    = ["sg_alb_shared_dev"] // Replace with your security group ID
+subnets            = ["subnet-03458dba469a5d163", "subnet-06d92c264a39f674d"] // Replace with your subnet IDs
 
-  enable_deletion_protection = false
+listener {
+port            = 443
+protocol        = "HTTPS"
+ssl_policy      = "ELBSecurityPolicy-2016-08"
+}
 
-  listener {
-    port            = 443
-    protocol        = "HTTPS"
-    ssl_policy      = "ELBSecurityPolicy-2016-08"
-  }
+listener {
+port            = 8099
+protocol        = "HTTPS"
+ssl_policy      = "ELBSecurityPolicy-2016-08"
+}
 
-  listener {
-    port            = 8099
-    protocol        = "HTTPS"
-    ssl_policy      = "ELBSecurityPolicy-2016-08"
-  }
+listener {
+port            = 80
+protocol        = "HTTP"
+}
 
-  listener {
-    port            = 80
-    protocol        = "HTTP"
-  }
-
-  listener {
-    port            = 8080
-    protocol        = "HTTP"
-  }
+listener {
+port            = 8080
+protocol        = "HTTP"
+}
 }

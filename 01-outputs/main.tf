532bd3affe3027339b12911f16616b693e873c33
Mukesh.Sahu <msahu201989@gmail.com>
2024-04-12 19:10:38 +0530

    Terraform

--- 01-outputs/main.tf
+++ 01-outputs/main.tf
@@ -133,21 +133,21 @@ resource "aws_lb_listener" "app_listener_8095" {
   }
 }
 
-resource "aws_lb_listener" "app_listener_80" {
-  load_balancer_arn = aws_lb.alb_shared_dev.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type = "redirect"
-
-    redirect {
-      protocol    = "HTTPS"
-      port        = "443"
-      status_code = "HTTP_301"
-    }
-  }
-}
+#resource "aws_lb_listener" "app_listener_80" {
+#  load_balancer_arn = aws_lb.alb_shared_dev.arn
+#  port              = 80
+#  protocol          = "HTTP"
+#
+#  default_action {
+#    type = "redirect"
+#
+#    redirect {
+#      protocol    = "HTTPS"
+#      port        = "443"
+#      status_code = "HTTP_301"
+#    }
+#  }
+#}
 
 resource "aws_lb_listener" "app_listener_8099" {
   load_balancer_arn = aws_lb.alb_shared_dev.arn
@@ -160,18 +160,18 @@ resource "aws_lb_listener" "app_listener_8099" {
   }
 }
 
-resource "aws_lb_listener" "app_listener_443" {
-  load_balancer_arn = aws_lb.alb_shared_dev.arn
-  port              = 443
-  protocol          = "HTTPS"
-
-  default_action {
-    type             = "fixed-response"
-
-    fixed_response {
-      content_type = "text/html"
-      message_body = "<h1>Error 503</h1> <p>This path is not configured in the listener rules</p> <p> - Shared Dev ALB</p>"
-      status_code  = "503"
-    }
-  }
-}
+#resource "aws_lb_listener" "app_listener_443" {
+#  load_balancer_arn = aws_lb.alb_shared_dev.arn
+#  port              = 443
+#  protocol          = "HTTPS"
+#
+#  default_action {
+#    type             = "fixed-response"
+#
+#    fixed_response {
+#      content_type = "text/html"
+#      message_body = "<h1>Error 503</h1> <p>This path is not configured in the listener rules</p> <p> - Shared Dev ALB</p>"
+#      status_code  = "503"
+#    }
+#  }
+#}
